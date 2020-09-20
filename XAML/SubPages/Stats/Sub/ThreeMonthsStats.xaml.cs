6eacfa7903b507e4cff972e824a8f908ced382c7
Alkanores <49155506+Alkanores@users.noreply.github.com>
2020-09-20 23:38:21 +0300

    fwef

--- XAML/SubPages/Stats/Sub/ThreeMonthsStats.xaml.cs
+++ XAML/SubPages/Stats/Sub/ThreeMonthsStats.xaml.cs
@@ -71,6 +71,8 @@ namespace Salary_Control.XAML.SubPages.Stats.Sub
                     ),
                     Palette = minusResourceCollection
                 });
+
+                finalCount.Text = countResult.totalCount.ToString();
             }
         }
     }
