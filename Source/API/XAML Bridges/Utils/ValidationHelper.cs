f82cb59d7079190b42801dbfaa764dd5c0f46824
Эдвард Гиголаев <49155506+movpushmov@users.noreply.github.com>
2021-09-09 18:51:13 +0300

    add event add & edit validation.

---
+++ Source/API/XAML Bridges/Utils/ValidationHelper.cs
@@ -0,0 +1,33 @@
+﻿using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml;
+
+namespace Budget_Control.Source.API.XAML_Bridges.Utils
+{
+    public enum ErrorType
+    {
+        FieldRequiredError,
+        EventInvalidCategory,
+        InvalidCost
+    }
+
+    public static class ValidationHelper
+    {
+        private static ResourceLoader _resourceLoader;
+
+        static ValidationHelper()
+        {
+            _resourceLoader = new ResourceLoader();
+        }
+
+        public static string GetErrorText(ErrorType type)
+    {
+            return _resourceLoader.GetString(type.ToString());
+        }
+
+        public static Visibility IsErrorBlockVisible(string error)
+        {
+            return !string.IsNullOrEmpty(error) && !string.IsNullOrWhiteSpace(error) ?
+                Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
